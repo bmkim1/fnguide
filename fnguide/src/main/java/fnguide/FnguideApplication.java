376532a4e2bb659d339a09b82b200077442cee24
bmkim <mudosa1101@gmail.com>
2023-12-14 14:33:46 +0900

    commit

---
+++ fnguide/src/main/java/fnguide/FnguideApplication.java
@@ -0,0 +1,13 @@
+package fnguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FnguideApplication.class, args);
+	}
+
+}
