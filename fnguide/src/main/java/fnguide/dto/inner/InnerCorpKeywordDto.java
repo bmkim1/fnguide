ebab0d4b35d4d0110eb20b9501ef2970e62ec59f
bmkim <mudosa1101@gmail.com>
2023-12-21 17:43:10 +0900

    a

---
+++ fnguide/src/main/java/fnguide/dto/inner/InnerCorpKeywordDto.java
@@ -0,0 +1,24 @@
+package fnguide.dto.inner;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InnerCorpKeywordDto {
+
+    private String enr;
+    private String companyName;
+    private String crn;
+    private String discoveredDate;
+    private Integer keywordSeq;
+    private String keyword;
+    private Integer companyInfoFreq;
+    private Integer patentFreq;
+    private Integer utilityFreq;
+    private Integer rndFreq;
+    private Integer homepageFreq;
+    private Integer innerKeywordFreq;
+    private Integer keywordRowCount;
+
+}
