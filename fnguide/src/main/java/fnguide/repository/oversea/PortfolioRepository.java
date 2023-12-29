c6d3e006a723397ae73366fd30a21771e7df6664
bmkim <mudosa1101@gmail.com>
2023-12-29 15:58:46 +0900

    해외 csv 변환로직

--- fnguide/src/main/java/fnguide/repository/oversea/PortfolioRepository.java
+++
@@ -1,9 +0,0 @@
-package fnguide.repository.oversea;
-
-import fnguide.entity.oversea.Portfolio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PortfolioRepository extends JpaRepository<Portfolio, String> {
-}
